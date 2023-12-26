0ddcc08226c445360895261b9361aaeb2e972f26
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-12-26 17:34:45 -0600

    Added: computed method

--- src/stores/bears/bears.store.ts
+++ src/stores/bears/bears.store.ts
@@ -12,6 +12,10 @@ interface BearState {
 
   bears: Bear[];
 
+  computed: {
+    totalBears: number;
+  };
+
   increaseBlackBears: (by: number) => void;
   increasePolarBears: (by: number) => void;
   increasePandaBears: (by: number) => void;
@@ -21,7 +25,7 @@ interface BearState {
   clearBears: () => void;
 }
 
-export const useBearStore = create<BearState>()((set) => ({
+export const useBearStore = create<BearState>()((set, get) => ({
   blackBears: 0,
   polarBears: 0,
   pandaBears: 0,
@@ -29,6 +33,12 @@ export const useBearStore = create<BearState>()((set) => ({
   bears: [
     { id: 1, name: 'Oso #1' }
   ],
+
+  computed: {
+    get totalBears(): number {
+      return get().blackBears + get().polarBears + get().pandaBears + get().bears.length;
+    }
+  },
 
   increaseBlackBears: (by: number) => set((state) => ({ blackBears: state.blackBears + by })),
   increasePolarBears: (by: number) => set((state) => ({ polarBears: state.polarBears + by })),
