34ca95a0e3801c77ee86af7b7dd3d447a79f5ef3
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-12-27 21:45:57 -0600

    Created: customSessionStorage

---
+++ src/stores/storages/session-storage.storage.ts
@@ -0,0 +1,16 @@
+import { StateStorage, createJSONStorage } from 'zustand/middleware';
+
+const storageAPI: StateStorage = {
+  getItem: function (name: string): string | Promise<string | null> | null {
+    const data = sessionStorage.getItem(name);
+    return data;
+  },
+  setItem: function (name: string, value: string): void {
+    sessionStorage.setItem(name, value);
+  },
+  removeItem: function (name: string): void | Promise<void> {
+    console.log('removeItem', name);
+  }
+}
+
+export const customSessionStorage = createJSONStorage(() => storageAPI);
