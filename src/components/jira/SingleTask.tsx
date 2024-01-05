304fdc6cc6d17c87c8daa6d67bceb30608641b91
Víctor Iván López González <victor_ivan2211@hotmail.com>
2024-01-05 14:53:28 -0600

    Added: draggable functionality

--- src/components/jira/SingleTask.tsx
+++ src/components/jira/SingleTask.tsx
@@ -1,19 +1,29 @@
 import { IoReorderTwoOutline } from 'react-icons/io5';
 import { Task } from '../../interfaces';
+import { useTaksStore } from '../../stores';
 
 interface Props {
   task: Task;
 }
 
 export const SingleTask = ({ task }: Props) => {
+
+  const setDraggingTaskId = useTaksStore((state) => state.setDraggingTaskId);
+  const removeDraggingTaskId = useTaksStore((state) => state.removeDraggingTaskId);
+
   return (
-    <div className="mt-5 flex items-center justify-between p-2">
+    <div
+      draggable
+      className="mt-5 flex items-center justify-between p-2"
+      onDragStart={() => setDraggingTaskId(task.id)}
+      onDragEnd={() => removeDraggingTaskId()}
+    >
       <div className="flex items-center justify-center gap-2">
         <p className="text-base font-bold text-navy-700">
           {task.title}
         </p>
       </div>
-      <span className=" h-6 w-6 text-navy-700 cursor-pointer">
+      <span className=" h-6 w-6 text-navy-700 cursor-grab">
         <IoReorderTwoOutline />
       </span>
     </div>
