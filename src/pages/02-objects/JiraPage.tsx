1f574bb3b79543fd7e879eeaef5b9da74611cc7d
Víctor Iván López González <victor_ivan2211@hotmail.com>
2024-01-04 21:12:14 -0600

    Added: <JiraTasks />

--- src/pages/02-objects/JiraPage.tsx
+++ src/pages/02-objects/JiraPage.tsx
@@ -1,26 +1,23 @@
 import { JiraTasks } from '../../components';
+import { useTaksStore } from '../../stores';
 
 export const JiraPage = () => {
+
+  const pendingTasks = useTaksStore((state) => state.getTaskByStatus('open'));
+  const inProgressTasks = useTaksStore((state) => state.getTaskByStatus('in-progress'));
+  const doneTasks = useTaksStore((state) => state.getTaskByStatus('done'));
+
   return (
     <>
       <h1>Tareas</h1>
-      <p>Manejo de estado con objectos de Zustand</p>
-      <hr />
-
+      <p className='border-b border-gray-300 p-2 mb-4'>Manejo de estado con objectos de Zustand</p>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-          
-          <JiraTasks title='Pendientes' value='pending' />
-          
-          <JiraTasks title='Avanzando' value='in-progress' />
-          
-          <JiraTasks title='Terminadas' value='done' />
+        <JiraTasks title='Pendientes' value='open' tasks={pendingTasks} />
 
+        <JiraTasks title='Avanzando' value='in-progress' tasks={inProgressTasks} />
+
+        <JiraTasks title='Terminadas' value='done' tasks={doneTasks} />
       </div>
-
-      
-
-
-
     </>
   );
 };
