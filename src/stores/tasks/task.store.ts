2ba3de5c3f6026bc0338e0071b0350b432b7a0ca
Víctor Iván López González <victor_ivan2211@hotmail.com>
2024-01-05 18:00:45 -0600

    Created: changeTaskStatus method

--- src/stores/tasks/task.store.ts
+++ src/stores/tasks/task.store.ts
@@ -9,6 +9,7 @@ interface TaskState {
   getTaskByStatus: (status: TaskStatus) => Task[];
   setDraggingTaskId: (taskId: string) => void;
   removeDraggingTaskId: () => void;
+  changeTaskStatus: (taskId: string, status: TaskStatus) => void;
 }
 
 const storeApi: StateCreator<TaskState> = (set, get) => ({
@@ -26,6 +27,18 @@ const storeApi: StateCreator<TaskState> = (set, get) => ({
 
   setDraggingTaskId: (taskId: string) => set({ draggingTaskId: taskId }),
   removeDraggingTaskId: () => set({ draggingTaskId: undefined }),
+
+  changeTaskStatus: (taskId: string, status: TaskStatus) => {
+    const task = get().tasks[taskId];
+    task.status = status;
+
+    set((state) => ({
+      tasks: {
+        ...state.tasks,
+        [taskId]: task,
+      }
+    }))
+  }
 });
 
 export const useTaksStore = create<TaskState>()(
