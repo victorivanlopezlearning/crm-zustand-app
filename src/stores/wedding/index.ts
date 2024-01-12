ab162ab9fb6983d185b60a745d01c3a66ca5b420
Víctor Iván López González <victor_ivan2211@hotmail.com>
2024-01-12 11:53:43 -0600

    Added: createDateSlice

--- src/stores/wedding/index.ts
+++ src/stores/wedding/index.ts
@@ -2,14 +2,16 @@ import { create } from 'zustand';
 import { devtools } from 'zustand/middleware';
 import { PersonSlice, createPersonSlice } from './person.slice';
 import { GuestSlice, createGuestSlice } from './guest.slice';
+import { DateSlice, createDateSlice } from './date.slice';
 
-type BoundState = PersonSlice & GuestSlice;
+type BoundState = PersonSlice & GuestSlice & DateSlice;
 
 export const useWeddingBoundStore = create<BoundState>()(
   devtools(
     (...a) => ({
       ...createPersonSlice(...a),
       ...createGuestSlice(...a),
+      ...createDateSlice(...a),
     })
   )
 )
