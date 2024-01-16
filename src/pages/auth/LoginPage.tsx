baaa6d01edd6f69a4fc668ff157f3e2756f67574
Víctor Iván López González <victor_ivan2211@hotmail.com>
2024-01-15 22:08:56 -0600

    Updated: styles and labels

--- src/pages/auth/LoginPage.tsx
+++ src/pages/auth/LoginPage.tsx
@@ -2,10 +2,10 @@ import { FormEvent } from 'react';
 
 export const LoginPage = () => {
 
-  const onSubmit = (event: FormEvent<HTMLFormElement> ) => {
+  const onSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     // const { username, password, remember } = event.target as HTMLFormElement;
-    const { username, password,remember } = event.target as typeof event.target & {
+    const { username, password, remember } = event.target as typeof event.target & {
       username: { value: string };
       password: { value: string };
       remember: { checked: boolean }
@@ -20,34 +20,36 @@ export const LoginPage = () => {
 
   return (
     <>
-      <h1 className="text-2xl font-semibold mb-4">Login</h1>
+      <h1 className="text-2xl font-semibold mb-4">Inicia sesión en tu cuenta</h1>
 
-      <form onSubmit={ onSubmit }>
+      <form onSubmit={onSubmit}>
 
         <div className="mb-4">
-          <label className="block text-gray-600">Username</label>
+          <label className="block text-gray-600">Nombre de usuario</label>
           <input type="text" name="username" autoComplete="off" />
         </div>
 
         <div className="mb-4">
-          <label className="block text-gray-600">Password</label>
+          <label className="block text-gray-600">Contraseña</label>
           <input type="password" name="password" autoComplete="off" />
         </div>
 
-        <div className="mb-4 flex items-center">
-          <input type="checkbox" name="remember" className="text-blue-500" />
-          <label className="text-gray-600 ml-2">Remember Me</label>
-        </div>
-        
-        <div className="mb-6 text-blue-500">
-          <a href="#" className="hover:underline">Forgot Password?</a>
+        <div className="mb-6 flex justify-between">
+          <div className='flex items-center'>
+            <input type="checkbox" name="remember" className="text-blue-500" />
+            <label className="text-gray-600 ml-2">Recordarme</label>
+          </div>
+
+          <div className=" text-blue-500">
+            <a href="#" className="hover:underline">¿Olvidaste la contraseña?</a>
+          </div>
         </div>
 
-        <button type="submit" className="bg-indigo-600">Login</button>
+        <button type="submit" className="bg-indigo-600">Iniciar sesión</button>
       </form>
-      <div className="mt-6 text-blue-500 text-center">
+      {/* <div className="mt-6 text-blue-500 text-center">
         <a href="#" className="hover:underline">Sign up Here</a>
-      </div>
+      </div> */}
     </>
   );
 };
