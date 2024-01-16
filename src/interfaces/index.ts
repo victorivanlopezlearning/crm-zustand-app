f8200fbad775bc00df627301f49e4e790ec842d1
Víctor Iván López González <victor_ivan2211@hotmail.com>
2024-01-16 17:08:05 -0600

    Created: User interface

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,1 +1,2 @@
 export * from './task.interface';
+export * from './user.interface';
---
+++ src/interfaces/user.interface.ts
@@ -0,0 +1,8 @@
+
+export interface User {
+  id: string;
+  email: string;
+  fullName: string;
+  isActive: boolean;
+  roles: string[];
+}
