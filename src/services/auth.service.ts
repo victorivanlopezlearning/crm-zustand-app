fe42b9b175e8e8c1927d68a6251dec884674cc47
Víctor Iván López González <victor_ivan2211@hotmail.com>
2024-01-16 21:36:58 -0600

    Created: AuthService

---
+++ src/services/auth.service.ts
@@ -0,0 +1,29 @@
+import { AxiosError } from 'axios';
+import { tesloApi } from '../api/teslo.api';
+
+export interface LoginResponse {
+  id: string;
+  email: string;
+  fullName: string;
+  isActive: boolean;
+  roles: string[];
+  token: string;
+}
+
+
+export class AuthService {
+
+  static login = async (email: string, password: string): Promise<LoginResponse> => {
+    try {
+      const { data } = await tesloApi.post<LoginResponse>('/auth/login', { email, password });
+      return data;
+    } catch (error) {
+      if (error instanceof AxiosError) {
+        console.log(error.response?.data);
+        throw new Error(error.response?.data);
+      }
+      console.log(error);
+      throw new Error('Unable to login');
+    }
+  }
+}
