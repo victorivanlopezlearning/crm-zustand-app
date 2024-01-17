7a41ebb16acbc3b4aaccfd9e193064400ad8d0b2
Víctor Iván López González <victor_ivan2211@hotmail.com>
2024-01-17 15:03:03 -0600

    Updated: styles

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -4,10 +4,9 @@ import { Outlet } from 'react-router-dom';
 export const AuthLayout = () => {
   return (
     <div className="bg-gray-100 flex justify-center items-center h-screen">
-      <div className="lg:p-36 md:p-52 sm:20 p-8 w-full lg:w-1/2">
+      <div className="w-full md:max-w-lg px-8">
         <Outlet />
       </div>
-      <div className="w-1/2 h-screen hidden lg:flex lg:flex-col items-center justify-center bg-indigo-700"></div>
     </div>
   );
 };
