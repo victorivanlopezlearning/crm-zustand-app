a558b8edbfe7bdbb7aa99b86b7c3a339ce8a1dc3
Víctor Iván López González <victor_ivan2211@hotmail.com>
2024-01-18 17:20:56 -0600

    Added: error functionality

--- src/stores/auth/auth.store.ts
+++ src/stores/auth/auth.store.ts
@@ -24,6 +24,7 @@ const storeApi: StateCreator<AuthState> = (set) => ({
       set({ status: 'authorized', token, user });
     } catch (error) {
       set({ status: 'unauthorized', token: undefined, user: undefined });
+      throw new Error('UnAuthorized');
     }
   },
 });
