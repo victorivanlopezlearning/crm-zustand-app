d1e88fdd946b9614d79df2a3e996e07f224edd23
Víctor Iván López González <victor_ivan2211@hotmail.com>
2024-01-18 13:30:06 -0600

    Created: interceptor request

--- src/api/teslo.api.ts
+++ src/api/teslo.api.ts
@@ -1,5 +1,19 @@
 import axios from 'axios';
+import { useAuthStore } from '../stores';
 
 export const tesloApi = axios.create({
   baseURL: 'http://localhost:3000/api'
 });
+
+tesloApi.interceptors.request.use(
+  (config) => {
+
+    const token = useAuthStore.getState().token;
+
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+    }
+
+    return config;
+  }
+)
