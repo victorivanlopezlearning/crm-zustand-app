a4e50981c9dce06e4581a49e603291162d077950
Víctor Iván López González <victor_ivan2211@hotmail.com>
2024-01-17 18:44:14 -0600

    Created: loginUser method

--- src/stores/auth/auth.store.ts
+++ src/stores/auth/auth.store.ts
@@ -1,20 +1,32 @@
 import { StateCreator, create } from 'zustand';
 import type { User } from '../../interfaces';
+import { AuthService } from '../../services/auth.service';
 
 export interface AuthState {
   status: AuthStatus;
   token?: string;
   user?: User;
+
+  loginUser: (email: string, password: string) => Promise<void>;
 }
 
 export type AuthStatus = 'authorized' | 'unauthorized' | 'pending';
 
-const storeApi: StateCreator<AuthState> = () => ({
+const storeApi: StateCreator<AuthState> = (set) => ({
   status: 'unauthorized',
   token: undefined,
   user: undefined,
+
+  loginUser: async (email: string, password: string) => {
+    try {
+      const { token, ...user } = await AuthService.login(email, password);
+      set({ status: 'authorized', token, user });
+    } catch (error) {
+      set({ status: 'unauthorized', token: undefined, user: undefined });
+    }
+  },
 });
 
 export const useAuthStore = create<AuthState>()(
   storeApi
-)
+);
