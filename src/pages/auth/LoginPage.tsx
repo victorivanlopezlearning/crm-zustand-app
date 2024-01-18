b297530d33d57a917f5dcff17adb6f1d345fb118
Víctor Iván López González <victor_ivan2211@hotmail.com>
2024-01-17 19:54:56 -0600

    Added: loginUser functionality

--- src/pages/auth/LoginPage.tsx
+++ src/pages/auth/LoginPage.tsx
@@ -1,6 +1,9 @@
 import { FormEvent } from 'react';
+import { useAuthStore } from '../../stores';
 
 export const LoginPage = () => {
+
+  const loginUser = useAuthStore((state) => state.loginUser);
 
   const onSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -12,9 +15,11 @@ export const LoginPage = () => {
     };
     console.log(username.value, password.value, remember.checked);
 
-    username.value = '';
-    password.value = '';
-    remember.checked = false;
+    loginUser(username.value, password.value);
+
+    // username.value = '';
+    // password.value = '';
+    // remember.checked = false;
   }
 
 
@@ -25,7 +30,7 @@ export const LoginPage = () => {
       <form onSubmit={onSubmit}>
 
         <div className="mb-4">
-          <label className="block text-gray-600">Nombre de usuario</label>
+          <label className="block text-gray-600">Email</label>
           <input type="text" name="username" autoComplete="off" />
         </div>
 
