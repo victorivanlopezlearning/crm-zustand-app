a2025752eddbd94394966f0c5a4dc924fc118551
Víctor Iván López González <victor_ivan2211@hotmail.com>
2024-01-19 12:09:53 -0600

    Created: checkAuthStatus functionality

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,7 +1,20 @@
 
-import { Outlet } from 'react-router-dom';
+import { Navigate, Outlet } from 'react-router-dom';
+import { useAuthStore } from '../stores';
+import { Spinner } from '../components';
 
 export const AuthLayout = () => {
+
+  const authStatus = useAuthStore((state) => state.status);
+  const checkAuthStatus = useAuthStore((state) => state.checkAuthStatus);
+
+  if(authStatus === 'pending') {
+    checkAuthStatus();
+    return <Spinner />
+  }
+
+  if(authStatus === 'authorized') return <Navigate to='/dashboard' />
+
   return (
     <div className="bg-gray-100 flex justify-center items-center h-screen">
       <div className="w-full md:max-w-lg px-8">
