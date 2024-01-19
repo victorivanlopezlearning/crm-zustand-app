be59fc1e932dc38eaeef8bb373b7c1068c2003c3
Víctor Iván López González <victor_ivan2211@hotmail.com>
2024-01-18 18:44:50 -0600

    Created: Spinner component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,6 @@
-
-
 export * from './jira/JiraTasks';
-
-
-export * from './shared/sidemenu/SideMenu';
+export * from './jira/SingleTask';
 export * from './shared/cards/WhiteCard';
+export * from './shared/loading/Spinner';
+export * from './shared/sidemenu/SideMenu';
+export * from './shared/sidemenu/SideMenuItem';
