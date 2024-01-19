27695fd04d2dc0c791f1792343052997a498ba0d
Víctor Iván López González <victor_ivan2211@hotmail.com>
2024-01-19 15:08:24 -0600

    Added: <RequestInfo />

--- src/pages/dashboard/DashboardPage.tsx
+++ src/pages/dashboard/DashboardPage.tsx
@@ -1,11 +1,12 @@
 import {
   IoAccessibilityOutline,
   IoHeartOutline,
+  IoInformationOutline,
   IoListOutline,
   IoLockClosedOutline,
   IoPawOutline
 } from 'react-icons/io5';
-import { WhiteCard } from '../../components';
+import { RequestInfo, WhiteCard } from '../../components';
 import { useBearStore, usePersonStore, useTaksStore } from '../../stores';
 
 export const Dashboard = () => {
@@ -56,10 +57,11 @@ export const Dashboard = () => {
           <p>Información</p>
         </WhiteCard>
 
-
-
+        <WhiteCard centered className='col-span-3'>
+          <IoInformationOutline size={50} className="text-indigo-600" />
+          <RequestInfo />
+        </WhiteCard>
       </div>
-
     </>
   );
 };
