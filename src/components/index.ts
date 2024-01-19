e236381a6c316fdbd8300f882814d88ab51d9b9b
Víctor Iván López González <victor_ivan2211@hotmail.com>
2024-01-19 15:08:34 -0600

    Created: RequestInfo component

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,3 +4,4 @@ export * from './shared/cards/WhiteCard';
 export * from './shared/loading/Spinner';
 export * from './shared/sidemenu/SideMenu';
 export * from './shared/sidemenu/SideMenuItem';
+export * from './shared/request-info/RequestInfo';
---
+++ src/components/shared/request-info/RequestInfo.tsx
@@ -0,0 +1,24 @@
+import { useEffect, useState } from 'react';
+import { tesloApi } from '../../../api/teslo.api';
+
+export const RequestInfo = () => {
+
+  const [info, setInfo] = useState<unknown>()
+
+  useEffect(() => {
+    tesloApi.get('/auth/private')
+      .then(res => setInfo(res.data))
+      .catch(() => setInfo('Error'));
+  }, [])
+
+  return (
+    <>
+      <h2>Información</h2>
+      <pre>
+        {
+          JSON.stringify(info, null, 2)
+        }
+      </pre>
+    </>
+  )
+}
