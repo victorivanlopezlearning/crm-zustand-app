7c2c0480d8c2c2002998e9b0fd1985120bb766d3
Víctor Iván López González <victor_ivan2211@hotmail.com>
2024-01-18 19:05:30 -0600

    Created: checkAuthStatus method

--- src/stores/auth/auth.store.ts
+++ src/stores/auth/auth.store.ts
@@ -9,18 +9,29 @@ export interface AuthState {
   user?: User;
 
   loginUser: (email: string, password: string) => Promise<void>;
+  checkAuthStatus: () => Promise<void>;
 }
 
 export type AuthStatus = 'authorized' | 'unauthorized' | 'pending';
 
 const storeApi: StateCreator<AuthState> = (set) => ({
-  status: 'unauthorized',
+  status: 'pending',
   token: undefined,
   user: undefined,
 
   loginUser: async (email: string, password: string) => {
     try {
       const { token, ...user } = await AuthService.login(email, password);
+      set({ status: 'authorized', token, user });
+    } catch (error) {
+      set({ status: 'unauthorized', token: undefined, user: undefined });
+      throw new Error('UnAuthorized');
+    }
+  },
+
+  checkAuthStatus: async () => {
+    try {
+      const { token, ...user } = await AuthService.checkStatus();
       set({ status: 'authorized', token, user });
     } catch (error) {
       set({ status: 'unauthorized', token: undefined, user: undefined });
